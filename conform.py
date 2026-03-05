07a4c12e9ecf8529fa2b5ebf3e3adf10494fc90c
Zeynep Göğüş <zeynep.gogus@tedbilisim.com.tr>
2026-03-05 14:37:42 +0300

    This script is written by Zeynep

---
+++ conform.py
@@ -0,0 +1,13 @@
+# This script is written by Zeynep
+
+def conform(value, min_val, max_val):
+    """Clamps a value between min and max."""
+    return max(min_val, min(max_val, value))
+
+def main():
+    print(conform(15, 0, 10))
+    print(conform(-5, 0, 10))
+    print(conform(7, 0, 10))
+
+if __name__ == "__main__":
+    main()
